f8d181f6ae291539544b3094d730b5f596b5fd54
Wojciech Białek <w.bialek@merixstudio.com>
2025-08-26 18:13:47 +0200

    Add simple Header component to the homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 
+import { PageHeader } from '../components/Header/Header';
+
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="">
+      <PageHeader
+        backUrl="/"
+        headerHeadline="Otwórz bramę"
+      />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">{children}</main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
@@ -19,8 +25,8 @@ export default function HomeLayout({
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
-            height={16}
+            width={15}
+            height={21}
           />
           Learn
         </a>
---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from 'next/image'
+
+export const PageHeader = ({
+  backUrl,
+  headerHeadline,
+}: {
+  backUrl?: string;
+  headerHeadline: string;
+}) => {
+  return (
+    <header
+      className="w-full flex items-start px-4 pt-[50px] pb-4 rounded-b-2xl relative overflow-hidden" 
+      style={{ 
+        background: 'linear-gradient(to top, #FADF99, #FFB034)',
+        color: 'var(--primary-blue)',
+        height: '280px',
+      }}
+    >
+      {backUrl && (
+        <Link 
+          href={backUrl} 
+          className="rounded-full border-3 p-1 mr-4 z-10"
+          style={{ borderColor: 'var(--primary-blue)' }}
+        >
+          <Image
+            src="/icons/back-arrow.svg"
+            alt="Back"
+            width={16}
+            height={21}
+          />
+        </Link>
+      )}
+      <h1 
+        className="text-[32px] leading-[40px] font-bold font-[family-name:var(--font-poppins)] whitespace-pre-line"
+        style={{ fontWeight: 700 }}
+      >
+        {headerHeadline.replace(' ', '\n')}
+      </h1>
+      
+      <div className="absolute bottom-2 right-0 z-0">
+        <Image
+          src="/images/keys.svg"
+          alt="Keys"
+          width={226}
+          height={230}
+        />
+      </div>
+    </header>
+  );
+};
